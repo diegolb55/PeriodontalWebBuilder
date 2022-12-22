d6bbd91c045c2bfa1680f21961034024c31fdf67
diegolb55 <diegolugobendezu@gmail.com>
2022-12-22 13:17:45 -0400

    lots of progress in Team page/widget

--- components/wpages/Equipo.js
+++ components/wpages/Equipo.js
@@ -29,27 +29,75 @@ export default function Equipo({noWidget, widgetControl, openWidget}){
                     </p>
                 </div>
             </div>
+
+
+
             <div className={styles.staff}>
-                <div>
-                    
+                <div className={styles.imgbox}>
+                    <div className={styles.imgcircle}>
+
+                    </div>
                 </div>
                 <div>
-                    <p>Doctor Name</p>
-                    <div>
-                        <p>  Periodoncista con X años de experiencia en enfermedades de encias e implantologia. Doctor en medicina dental con educacion en Boston University Salud y Dental Medicine 1988-1992.</p>
-                        <p>  En el campo de especialidad y New Jersey 2000 (Currently Rudgers University). Afan de las artes finas, los viajes y la arquitectura </p>
+                    <div className={styles.infobox}>
+                        <p className={styles.name}>Doctor Name</p>
+                        <div className={styles.infodivs}>
+                            <div className={styles.info}>
+                                <p>   Periodoncista con X años de experiencia en enfermedades de encias e implantologia. Doctor en medicina dental con educacion en Boston University Salud y Dental Medicine 1988-1992.</p>
+                            </div>
+                            <div className={styles.info}>
+                                <p>  En el campo de especialidad y New Jersey 2000 (Currently Rudgers University). Afan de las artes finas, los viajes y la arquitectura</p>
+                            </div>
+                        </div>
+                    </div>
+                    
+                </div>
+            </div>
+            <div className={`${styles.staff} ${styles.staffReverse}`}>
+                <div className={`${styles.imgbox} ${styles.imgbox2}`}>
+                    <div className={styles.imgcircle}>
+
+                    </div>
+                </div>
+                <div>
+                    <div className={styles.infobox}>
+
+                        <p className={`${styles.name} ${styles.leftify}`}>Doctor Name</p>
+
+                        <div className={styles.infodivs}>
+                            <div className={`${styles.info} ${styles.leftify}`}>
+                                <p>   Periodoncista con X años de experiencia en enfermedades de encias e implantologia. Doctor en medicina dental con educacion en Boston University Salud y Dental Medicine 1988-1992.</p>
+                            </div>
+                            <div className={`${styles.info} ${styles.leftify2}`} style={{border: "none"}}>
+                                <p>  En el campo de especialidad y New Jersey 2000 (Currently Rudgers University). Afan de las artes finas, los viajes y la arquitectura</p>
+                            </div>
+                        </div>
                     </div>
                     
                 </div>
             </div>
             <div className={styles.staff}>
-                <div></div>
-                <div></div>
+                <div className={styles.imgbox}>
+                    <div className={styles.imgcircle}>
+
+                    </div>
+                </div>
+                <div>
+                    <div className={styles.infobox}>
+                        <p className={styles.name}>Doctor Name</p>
+                        <div className={styles.infodivs}>
+                            <div className={styles.info}>
+                                <p>   Periodoncista con X años de experiencia en enfermedades de encias e implantologia. Doctor en medicina dental con educacion en Boston University Salud y Dental Medicine 1988-1992.</p>
+                            </div>
+                            <div className={styles.info}>
+                                <p>  En el campo de especialidad y New Jersey 2000 (Currently Rudgers University). Afan de las artes finas, los viajes y la arquitectura</p>
+                            </div>
+                        </div>
+                    </div>
+                    
+                </div>
             </div>
-            <div className={styles.staff}>
-                <div></div>
-                <div></div>
-            </div>
+            
 
         
         </div>  
@@ -92,17 +140,95 @@ export default function Equipo({noWidget, widgetControl, openWidget}){
            
             <OpenContent isOpen={widgetControl?.current.equipo} >
                 <div className={styles.openContent}>
-                    <div className={styles.history}></div>
-                    <div className={styles.staff}>
-                        <div></div>
-                        <div></div>
+                    <div className={styles.history}>
+                        <div>
+                            <Image
+                                src="/images/dentalg.jpg"
+                                alt=""
+                                layout="fill"
+                                objectFit='cover'
+
+                            />
+                        </div>
+                        <div>
+                            <h3>Historia</h3>
+                            <p>
+                                Oficina ofreciendo tratamientos a pacientes con enfermedades periodontales e implantologia oral desde el año 2000.
+                            </p>
+                        </div>
                     </div>
-                    <div className={styles.staff}></div>
-                    <div className={styles.staff}></div>
 
-                    
 
-                </div>  
+            
+                    <div className={styles.staff}>
+                        <div className={styles.imgbox}>
+                            <div className={styles.imgcircle}>
+
+                            </div>
+                        </div>
+                        <div>
+                            <div className={styles.infobox}>
+                                <p className={styles.name}>Doctor Name</p>
+                                <div className={styles.infodivs}>
+                                    <div className={styles.info}>
+                                        <p>   Periodoncista con X años de experiencia en enfermedades de encias e implantologia. Doctor en medicina dental con educacion en Boston University Salud y Dental Medicine 1988-1992.</p>
+                                    </div>
+                                    <div className={styles.info}>
+                                        <p>  En el campo de especialidad y New Jersey 2000 (Currently Rudgers University). Afan de las artes finas, los viajes y la arquitectura</p>
+                                    </div>
+                                </div>
+                            </div>
+                            
+                        </div>
+                    </div>
+                    <div className={`${styles.staff} ${styles.staffReverse}`}>
+                        <div className={`${styles.imgbox} ${styles.imgbox2}`}>
+                            <div className={styles.imgcircle}>
+
+                            </div>
+                        </div>
+                        <div>
+                            <div className={styles.infobox}>
+
+                                <p className={`${styles.name} ${styles.leftify}`}>Doctor Name</p>
+
+                                <div className={styles.infodivs}>
+                                    <div className={`${styles.info} ${styles.leftify}`}>
+                                        <p>   Periodoncista con X años de experiencia en enfermedades de encias e implantologia. Doctor en medicina dental con educacion en Boston University Salud y Dental Medicine 1988-1992.</p>
+                                    </div>
+                                    <div className={`${styles.info} ${styles.leftify2}`} style={{border: "none"}}>
+                                        <p>  En el campo de especialidad y New Jersey 2000 (Currently Rudgers University). Afan de las artes finas, los viajes y la arquitectura</p>
+                                    </div>
+                                </div>
+                            </div>
+                            
+                        </div>
+                    </div>
+                    <div className={styles.staff}>
+                        <div className={styles.imgbox}>
+                            <div className={styles.imgcircle}>
+
+                            </div>
+                        </div>
+                        <div>
+                            <div className={styles.infobox}>
+                                <p className={styles.name}>Doctor Name</p>
+                                <div className={styles.infodivs}>
+                                    <div className={styles.info}>
+                                        <p>   Periodoncista con X años de experiencia en enfermedades de encias e implantologia. Doctor en medicina dental con educacion en Boston University Salud y Dental Medicine 1988-1992.</p>
+                                    </div>
+                                    <div className={styles.info}>
+                                        <p>  En el campo de especialidad y New Jersey 2000 (Currently Rudgers University). Afan de las artes finas, los viajes y la arquitectura</p>
+                                    </div>
+                                </div>
+                            </div>
+                            
+                        </div>
+                    </div>
+            
+
+        
+        </div> 
             </OpenContent>
               
             
